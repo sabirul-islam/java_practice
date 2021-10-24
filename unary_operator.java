cecfaf1ea3b639422cef4cd958c1933c4c1e2264
sabirul-islam <sabirul.shimul@gmail.com>
2021-10-24 08:22:07 +0600

    unary operator

---
+++ unary_operator.java
@@ -0,0 +1,18 @@
+public class unary_operator {
+ public static void main(String[] args) {
+     int x = 25;
+     int y;
+
+     y = --x;
+     System.out.println(y);
+
+     y = x--;
+     System.out.println(y);
+
+     y = ++x;
+     System.out.println(y);
+
+     y = x++;
+     System.out.println(y);
+ }   
+}
