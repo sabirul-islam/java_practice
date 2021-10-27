15c416f4046284f19becc01790a3823473d49b9a
sabirul-islam <sabirul.shimul@gmail.com>
2021-10-27 09:09:57 +0600

    bitwise operator

---
+++ bitwise_operator.java
@@ -0,0 +1,22 @@
+public class bitwise_operator {
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 12;
+        int c;
+
+        c = a & b;
+        System.out.println("a&b = "+c);
+
+        c = a | b;
+        System.out.println("a|b = "+c);
+
+        c = a ^ b;
+        System.out.println("a^b = "+c);
+
+        c = a >> 3;
+        System.out.println("a>>b = "+c);
+
+        c = a << 3;
+        System.out.println("a<<b = "+c);
+    }
+}
