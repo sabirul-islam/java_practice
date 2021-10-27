f730485d251d0e826974637598ce5fd6db225d4b
sabirul-islam <sabirul.shimul@gmail.com>
2021-10-27 09:19:46 +0600

    math

---
+++ math.java
@@ -0,0 +1,18 @@
+public class math {
+    public static void main(String[] args) {
+        int a = -25;
+        int b = 2;
+        int c = 3;
+        int x = 20;
+        int y = 10;
+        double z = 10.4;
+
+        System.out.println("Maximum number: " + Math.max(x,y));
+        System.out.println("Minimum number: " + Math.min(x,y));
+        System.out.println("Absolute number: " + Math.abs(a));
+        System.out.println("Power: " + Math.pow(b,c));
+        System.out.println("Round: " + Math.round(z));
+        System.out.println("PI: " + Math.PI);
+
+    }
+}
