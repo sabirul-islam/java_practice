d7a8e199826f1ea226acec0438e2019e87b2ec40
sabirul-islam <sabirul.shimul@gmail.com>
2021-10-27 09:25:14 +0600

    for loop

---
+++ for_loop.java
@@ -0,0 +1,13 @@
+public class for_loop {
+    /*
+    int i = 1 => initialization;
+    i <= 10 => condition;
+    i++ => increment;
+    i-- => Decrement;
+    */
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
