5b7acb2ae0b20e99bd02b2b82227ebf2f4038596
sabirul-islam <sabirul.shimul@gmail.com>
2021-10-28 06:14:51 +0600

    break

--- break_statement.java
+++ break_statement.java
@@ -1,11 +1,9 @@
 public class break_statement {
     public static void main(String[] args) {
         for(int i = 1; i <= 10; i++){
-            if(i == 5){
-                System.out.println(i);
+            if(i == 5)
                 break;
-            }
-
+                System.out.println(i);
         }
     }
 }
