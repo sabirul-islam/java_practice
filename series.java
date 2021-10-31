da4bfa6643c888bf4d619b2cb7fd4bfcf1ea2bfa
sabirul-islam <sabirul.shimul@gmail.com>
2021-10-31 06:23:12 +0600

    Series

--- series.java
+++ series.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class series {
     public static void main(String[] args) {
-        //1+2+3+......+n;
         Scanner input = new Scanner(System.in);
-
         int sum = 0, n;
-
         System.out.print("Enter last number: ");
         n = input.nextInt();
 
+        //1+2+3+......+n;
         for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+        //1+3+5+.......n;
+        for (int i = 1; i <= n; i=i+2) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+        for (int i = 2; i <= n; i = i+2){
             sum = sum + i;
         }
         System.out.println(sum);
