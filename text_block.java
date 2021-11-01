e80ac9b63a9dca35d1123e1d48681a1b40ed43e4
sabirul-islam <sabirul.shimul@gmail.com>
2021-11-01 21:02:32 +0600

    text block

---
+++ text_block.java
@@ -0,0 +1,9 @@
+public class text_block {
+    public static void main(String[] args) {
+        String paragraph = """
+        The roaring fans saw
+        their beloved "tigers" create another
+        \t bit of history in their favourite
+        den last night.""";
+    }
+}
