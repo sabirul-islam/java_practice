87d462bd3a6fe3f03a30b06169baf5a4a672c622
sabirul-islam <sabirul.shimul@gmail.com>
2021-11-02 07:01:00 +0600

    underscore in numeric literal

---
+++ numeric_literal_underscore.java
@@ -0,0 +1,13 @@
+public class numeric_literal_underscore {
+    public static void main(String[] args) {
+        /*
+        int x1 = 2_101_969; //one more underscore in integer;
+        int x2 = 1__192; //one by one underscore
+        int x3 = 03_661; //underscore in octal literal;
+        int x4 = 0b0111_1011_0001; //underscore in binary literal;
+        int x5 = 0x7_B_1; //underscore in hexadecimal literal;
+        byte b1 = 1_2_7; //underscore in bite literal;
+        double d1 = 1_29.09_19; //underscore in double literal;
+        */
+    }
+}
