1725a28a645d023b9a2953d36d9ad5867169a0e8
sabirul-islam <sabirul.shimul@gmail.com>
2021-11-02 07:06:29 +0600

    data_type_default_value

---
+++ data_type_default_value.java
@@ -0,0 +1,14 @@
+public class data_type_default_value {
+    public static void main(String[] args) {
+        /*
+        byte => 0;
+        short => 0;
+        int => 0;
+        long => 0L;
+        float => 0.0f;
+        double => 0.0d;
+        char => '\u0000';
+        boolean => false;
+        */
+    }
+}
