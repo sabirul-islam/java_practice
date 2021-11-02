a67245fb635f4baa03654178e7076a414639c2aa
sabirul-islam <sabirul.shimul@gmail.com>
2021-11-02 07:38:33 +0600

    data type, memory size, range

---
+++ TypeSizeRange.java
@@ -0,0 +1,13 @@
+public class TypeSizeRange {
+    public static void main(String[] args) {
+        /*
+        byte => 8 bit or 1 byte => -128 to 127;
+        short => 16 bit or 2 byte => -32,768 to 32,767;
+        char 16 bit or 2 byte => 0 to 65,535;
+        int => 32 bit or 4 byte => -2,147,483,648 to 2,147,483,647;
+        long => 64 bit or 8 byte => -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807;
+        float => 32 bit or 4 byte =>1.40239846 * 10 (pow) -45 to 3.40282347 * 10 pow 38;
+        double 64 bit or 8 byte => 4.940656458412465 * 10 (pow) -324 to 1.7976931348623157 *(pow) 308;
+        */
+    }
+}
