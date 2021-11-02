30b7362828ab1473c4a24a3e0acbfe93cfb129a1
sabirul-islam <sabirul.shimul@gmail.com>
2021-11-02 06:51:07 +0600

    char to ascii value

---
+++ ascii_value.java
@@ -0,0 +1,7 @@
+public class ascii_value {
+    public static void main(String[] args) {
+        char a = 'a';
+        int asciiValue = a;
+        System.out.println("ascii value of a: "+asciiValue);
+    }
+}
